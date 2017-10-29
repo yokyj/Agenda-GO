ab8b42635f45c765ad6af66816920bc3749095e1
toupiOfRivia <453040945@qq.com>
2017-10-29 06:39:15 -0700

    meeting set user

--- entity/main.go
+++ entity/main.go
@@ -11,9 +11,9 @@ func main() {
 	parti = append(parti, "qqq")
 	parti = append(parti, "www")
 	parti = append(parti, "eee")
-	t1, _ := time.Parse("2006-01-02 15:04:05", "2017-10-29 07:37:18")
-	t2, _ := time.Parse("2006-01-02 15:04:05", "2017-10-29 08:37:28")
-	err := meeting.CreateMeeting("meet2", parti, t1, t2)
+	t1, _ := time.Parse("2006-01-02 15:04:05", "2017-10-29 08:37:18")
+	t2, _ := time.Parse("2006-01-02 15:04:05", "2017-10-29 09:37:28")
+	err := meeting.CreateMeeting("meet1", parti, t1, t2)
 	fmt.Println(err)
 	meeting.WriteMeetingInfo()
 }
